7609ab101faa90a05c3504968a036eaa039ea9ba
Frederick D <FrederickDelta@gmail.com>
2020-12-23 11:35:31 +0800

    06

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -1,37 +1,11 @@
+import ProjectSummary from "./ProjectSummary"
+
 function ProjectList() {
   return (
     <div className="project-list">
-      <div className="project-summary card">
-        <div className="card-header">
-          <div className="card-header-title">
-            <p>Project Title</p>
-          </div>
-        </div>
-        <div className="card-content">
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui tempora error numquam sit aperiam ut eos nisi nesciunt officiis quibusdam.</p>
-        </div>
-        <div className="card-footer">
-          <div className="card-footer-item has-text-grey">
-            <p>Posted by Eric, 2020-12-23 10:56</p>
-          </div>
-        </div>
-      </div>
-
-      <div className="project-summary card">
-        <div className="card-header">
-          <div className="card-header-title">
-            <p>Project Title</p>
-          </div>
-        </div>
-        <div className="card-content">
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui tempora error numquam sit aperiam ut eos nisi nesciunt officiis quibusdam.</p>
-        </div>
-        <div className="card-footer">
-          <div className="card-footer-item has-text-grey">
-            <p>Posted by Eric, 2020-12-23 10:56</p>
-          </div>
-        </div>
-      </div>
+      <ProjectSummary />
+      <ProjectSummary />
+      <ProjectSummary />
     </div>
   )
 }
---
+++ src/components/projects/ProjectSummary.js
@@ -0,0 +1,24 @@
+function ProjectSummary() {
+  return (
+    <div className="project-summary card mb-3">
+      <div className="card-header">
+        <div className="card-header-title">
+          <p>Project Title</p>
+        </div>
+      </div>
+      <div className="card-content">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui tempora error numquam sit aperiam ut eos nisi nesciunt officiis quibusdam.</p>
+      </div>
+      <div className="card-footer">
+        <div className="card-footer-item has-text-grey">
+          <p>2020-12-23 10:56</p>
+        </div>
+        <div className="card-footer-item">
+          <p>Posted by Eric</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ProjectSummary
